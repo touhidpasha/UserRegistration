034474493a3a13ddd9a5a770d0ab80e173911fb4
touhidpasha <touhidpasha552@gmail.com>
2021-09-05 18:32:31 +0530

    last name validated

--- src/MyPackage/MainClass.java
+++ src/MyPackage/MainClass.java
@@ -6,9 +6,14 @@ public class MainClass {
 	
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-	UserRegistration u1=new UserRegistration();
+	UserRegistration u1=new UserRegistration();//creating object
+	
 	System.out.println("Enter your firstName");
 	while(!u1.firstNameValidation(sc.nextLine())) {}
+	
+	System.out.println("Enter your lastName");
+	while(!u1.lastNameValidation(sc.nextLine())) {}
+	
 	
 	
 	
--- src/MyPackage/UserRegistration.java
+++ src/MyPackage/UserRegistration.java
@@ -1,8 +1,8 @@
 package MyPackage;
 public class UserRegistration {
 	
-public String firstName;
-public boolean firstNameValidation(String name) {
+public String firstName,lastName;
+public boolean firstNameValidation(String name) {//function for validation first name
 	if(name.length()<3)
 	{	System.out.println("name must have atleast 3 characters ");
 		return(false);}
@@ -15,6 +15,20 @@ public boolean firstNameValidation(String name) {
 	return true;
 			
 	
+}
+
+public boolean lastNameValidation(String name) {//function for validation last name
+	if(name.length()<3)
+	{	System.out.println("name must have atleast 3 characters ");
+		return(false);}
+	
+	if(!(name.charAt(0)>=65 && name.charAt(0)<=90))
+		{System.out.println("name should start with capital letter ");
+		return false;
+		}
+	lastName=name;
+	return true;
+			
 }
 
 }
